f96fa0303a199cea9ea6f6bac853727b95035634
Lihe Wang <wanglihe2010@gmail.com>
2020-01-05 17:51:19 -0600

    initial POM and set POM parent

---
+++ src/main/java/com/example/onlineshoppingproject/OnlineShoppingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlineshoppingproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShoppingProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineShoppingProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/onlineshoppingproject/OnlineShoppingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlineshoppingproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShoppingProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
