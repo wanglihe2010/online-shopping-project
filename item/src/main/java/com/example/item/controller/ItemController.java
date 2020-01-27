0652cf74cfa1ad226c949208da4a4fb7c984f47e
Wang <836551@cognizant.com>
2020-01-27 15:40:54 -0600

    add actuator

--- item/src/main/java/com/example/item/controller/ItemController.java
+++ item/src/main/java/com/example/item/controller/ItemController.java
@@ -6,11 +6,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RefreshScope
 @RequestMapping(value = "item")
 public class ItemController {
 
