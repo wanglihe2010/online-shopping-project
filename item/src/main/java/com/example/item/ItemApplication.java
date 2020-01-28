ade0daec5ec0d0d5c7938913f72efb0e787d2de3
Wang <836551@cognizant.com>
2020-01-28 17:38:49 -0600

    comment out redis and add log

--- item/src/main/java/com/example/item/ItemApplication.java
+++ item/src/main/java/com/example/item/ItemApplication.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableCaching
+//@EnableCaching
 public class ItemApplication {
 
 	public static void main(String[] args) {
